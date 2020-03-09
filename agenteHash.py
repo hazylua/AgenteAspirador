07761545b621c949e5a17eb0c314177f4c3ce5f2
yb00 <yagobraz98@gmail.com>
2020-03-08 23:01:40 -0300

    Adicionar agenteHash.py

---
+++ agenteHash.py
@@ -0,0 +1,16 @@
+# Estados podem ser mapeados em um vetor por hash.
+Estados = {'0': {'a': '1', 'b': '0', 'c': '-1'},
+           '1': {'a': '2', 'b': '0', 'c': '0'},
+           '2': {'a': '0', 'b': '0', 'c': '1'}}
+print(Estados)
+Posicao = '0'
+Cadeia = '0'
+while len(Cadeia) != 0:
+    Cadeia = input()
+    for acao in Cadeia:
+        Posicao = Estados[Posicao][acao]
+        if(Posicao == '-1'):
+            print("Impossível.")
+            break
+    print(Posicao)
+    Posicao = '0'
